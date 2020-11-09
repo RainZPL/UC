8a24c8ff3f547c5dc951f28a9fc0f81b6130f33b
RainZPL <m15511138585@163.com>
2020-11-09 17:18:17 +0000

    day1,2

---
+++ UNIXC01/动态库静态库(dynamic static library)/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "print.h"
+
+int main(){
+
+	diamond();
+	
+	return 0;
+}
---
+++ UNIXC01/动态库静态库(dynamic static library)/print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "print.h"
+
+void diamond()
+{
+    printf("     *\n");
+    printf("   *   *\n");
+    printf(" *       *\n");
+    printf("   *   *\n");
+    printf("     *\n");
+}
+
+
